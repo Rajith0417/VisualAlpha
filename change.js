bdda3e17b735de2d040ff97ff475d4dcde86a355
Raji G <rajith0417@gmail.com>
2023-01-15 22:50:59 +0530

    logic completed

--- change.js
+++ change.js
@@ -1,6 +1,20 @@
-
-export class Change {
-  calculate(coinArray, target) {
-    throw new Error("Remove this statement and implement this function");
-  }
-}
+function countCoins(amount)
+    {
+        let notes = [ 100, 25, 10, 5, 1 ];
+        let coinsCounter = Array(5).fill(0);
+        
+        // count coins using Greedy approach
+        for (let i = 0; i < 5; i++) {
+            if (amount >= notes[i]) {
+                coinsCounter[i] = Math.floor(amount / notes[i]);
+                amount = amount % notes[i];
+            }
+        }
+        console.log(coinsCounter);
+    }
+     
+ 
+// driver code
+ 
+    let amount = 400;
+    countCoins(amount);
