51495698b7a3876da1fc5ae6d319f2579a8828a4
Raji G <rajith0417@gmail.com>
2023-01-15 22:53:44 +0530

    user input added

--- change.js
+++ change.js
@@ -12,9 +12,13 @@ function countCoins(amount)
         }
         console.log(coinsCounter);
     }
-     
- 
-// driver code
- 
-    let amount = 400;
-    countCoins(amount);
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Enter Amount', amount => {
+  countCoins(amount);
+  readline.close();
+});
